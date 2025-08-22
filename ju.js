3ada23e4ce365f125530aab5d4ca91a2e648eb42
Vũ Hảo <hta022563@gmail.com>
2025-08-23 03:24:34 +0700

    first commit

---
+++ ju.js
@@ -0,0 +1,26 @@
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 35,
+        density: { enable: true, value_area: 800 },
+      },
+      color: { value: "#8BF0F6" },
+      shape: { type: "star" },
+      opacity: { value: 0.7, random: true },
+      size: { value: 3, random: true },
+      line_linked: {
+        enable: true,
+        distance: 100,
+        color: "#8BF0F6",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 1.5,
+        direction: "none",
+        random: true,
+        out_mode: "out",
+      },
+    },
+  });
