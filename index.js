09d4ccf5155030cf5ac6510ad890600cdd358cc5
Vũ Hảo <hta022563@gmail.com>
2025-08-27 13:56:51 +0700

    add Responsive taskbar

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function toggleMenu() {
-  const menu = document.getElementById("menu");
-  menu.classList.toggle("hidden");
-}
+let MenuTogle = document.getElementById("menu-togle");
+let MoblieNav = document.getElementById("mobile-nav");
+let BarIcon = document.getElementById("bar-icon");
+let CloseIcon = document.getElementById("close-icon");
+
+MenuTogle.addEventListener("click", function(){
+  MoblieNav.classList.toggle("active");
+  if(MoblieNav.classList.contains("active")){
+    BarIcon.style.display = "none";
+    CloseIcon.style.display  = "block";
+  }
+  else {
+    BarIcon.style.display = "block";
+    CloseIcon.style.display = "none";
+  }
+});
